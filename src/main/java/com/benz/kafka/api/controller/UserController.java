70ccce8c092517351abd5ee46a87534efc21144b
Nafaz Benzema <34432437+Benzeman97@users.noreply.github.com>
2020-10-19 09:37:06 +0530

    crossorigin is implemented

--- src/main/java/com/benz/kafka/api/controller/UserController.java
+++ src/main/java/com/benz/kafka/api/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins="*",maxAge=3600)
 @RestController
 @RequestMapping("/kafka")
 public class UserController {
